82224d16f0ce05a84c0548492b46b7da6f56cae7
PLM <lopezpatri@hotmail.com>
2017-01-26 11:29:29 +0100

    Añadida línea sin conflicto

--- Proyecto2/src/es/unican/is2/proyecto2/Clase2.java
+++ Proyecto2/src/es/unican/is2/proyecto2/Clase2.java
@@ -5,5 +5,6 @@ public class Clase2 {
 	public static void main(String args[]) {
 		
 		System.out.println("Clase 2 del repositorio 1 - 11:20");
+		System.out.println("Esto no debería generar conflicto");
 	}
 }
