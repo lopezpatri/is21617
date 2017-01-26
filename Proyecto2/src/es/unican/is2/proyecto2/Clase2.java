7046c361d30a54b4eff77c895cac54a46b141af6
PLM <lopezpatri@hotmail.com>
2017-01-26 11:22:11 +0100

    Commint  Rep1 11:21

--- Proyecto2/src/es/unican/is2/proyecto2/Clase2.java
+++ Proyecto2/src/es/unican/is2/proyecto2/Clase2.java
@@ -3,7 +3,7 @@ package es.unican.is2.proyecto2;
 public class Clase2 {
 
 	public static void main(String args[]) {
-		
-		System.out.println("Clase 2 del repositorio 1");
+		System.out.println("Clase 2 del repositorio 1 - 11:21");
 	}
+	
 }
