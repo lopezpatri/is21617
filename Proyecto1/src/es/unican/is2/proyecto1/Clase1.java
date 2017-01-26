b875a652c220c5efe2db1a1f6c9a3968ef799a6b
PLM <lopezpatri@hotmail.com>
2017-01-26 11:20:56 +0100

    Commit 11:20

--- Proyecto1/src/es/unican/is2/proyecto1/Clase1.java
+++ Proyecto1/src/es/unican/is2/proyecto1/Clase1.java
@@ -5,6 +5,6 @@ public class Clase1 {
 
 	public static void main(String args[]) {
 
-		System.out.println("Clase 1 del repositorio 1");
+		System.out.println("Clase 1 del repositorio 1 - 11:20");
 	}
 }
