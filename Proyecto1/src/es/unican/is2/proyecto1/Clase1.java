ffa99720f6886c2faf541372f65ff9d5fa2dd305
PLM <lopezpatri@hotmail.com>
2017-01-26 11:33:29 +0100

    commit 11:33

--- Proyecto1/src/es/unican/is2/proyecto1/Clase1.java
+++ Proyecto1/src/es/unican/is2/proyecto1/Clase1.java
@@ -6,5 +6,7 @@ public class Clase1 {
 	public static void main(String args[]) {
 
 		System.out.println("Clase 1 del repositorio 1 - 11:20");
+		System.out.println("ESto no genera conflicto");
+		
 	}
 }
